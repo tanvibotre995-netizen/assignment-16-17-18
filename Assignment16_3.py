8c46930684dd3d4312ae8655cbae9885a760d912
tanvibotre995-netizen <tanvibotre995@gmail.com>
2026-07-06 21:19:23 +0530

    Create Assignment16_3.py

---
+++ Assignment16_3.py
@@ -0,0 +1,15 @@
+def Add(No1,No2):
+    Add=No1+No2
+    print("addition is",Add)
+    return No1+No2
+
+def main():
+    No1=int(input("enter number:"))
+    No2=int(input("enter number:"))
+
+    Add(No1,No2)
+
+if __name__ == "__main__":
+    main()
+
+
