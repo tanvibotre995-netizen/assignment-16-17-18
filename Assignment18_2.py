0bd2d41ab6fa033f67ae616bf81254d7abf6fb96
tanvibotre995-netizen <tanvibotre995@gmail.com>
2026-07-08 22:29:09 +0530

    Create Assignment18_2.py

---
+++ Assignment18_2.py
@@ -0,0 +1,14 @@
+def maximum(data):
+    return max(data)
+
+def main():
+    data= [20,45,79,56,90]
+
+    print("list is",data)
+
+    result=maximum(data)
+
+    print("maximum number is",result)
+
+if __name__ == "__main__":
+    main()
