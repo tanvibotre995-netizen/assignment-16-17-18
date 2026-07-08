ba45cc9fefbb11d6875016ad4f3f52bb8478b270
tanvibotre995-netizen <tanvibotre995@gmail.com>
2026-07-08 22:31:25 +0530

    Create Assignment18_3.py

---
+++ Assignment18_3.py
@@ -0,0 +1,14 @@
+def minimum(data):
+    return min(data)
+
+def main():
+    data= [20,45,79,56,90]
+
+    print("list is",data)
+
+    result=minimum(data)
+
+    print("minimum number is",result)
+
+if __name__ == "__main__":
+    main()
