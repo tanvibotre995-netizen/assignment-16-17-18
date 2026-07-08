db44e44dae6924c50f466f8e046708e2b0eae421
tanvibotre995-netizen <tanvibotre995@gmail.com>
2026-07-08 18:19:15 +0530

    Create Arithmetic.py

---
+++ Arithmetic.py
@@ -0,0 +1,19 @@
+def Addition(No1,No2):
+    Ans=0
+    Ans= No1+No2
+    return Ans
+
+def Substraction(No1,No2):
+    Ans=0
+    Ans= No1-No2
+    return Ans
+
+def Multiplication(No1,No2):
+    Ans=0
+    Ans= No1*No2
+    return Ans
+
+def Division(No1,No2):
+    Ans=0
+    Ans= No1/No2
+    return Ans
