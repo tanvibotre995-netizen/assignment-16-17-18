432df9251a5d8f6d9bb95490415407225af99fcc
tanvibotre995-netizen <tanvibotre995@gmail.com>
2026-07-08 18:54:18 +0530

    Create Assignment17_7.py

---
+++ Assignment17_7.py
@@ -0,0 +1,12 @@
+def pattern():
+    for i in range(5):
+        for j in range(1, 6):
+            print(j, end="")
+        print()
+
+def main():
+    pattern()
+
+if __name__ == "__main__":
+    main()
+    
